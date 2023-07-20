0266488390c7b0f5c1d4912bbdf519d28ae84100
Jiwon Lee <leejwblack@snu.ac.kr>
2023-07-20 16:33:14 +0900

    add rotation class (#15)

--- tutorial-03/transform.py
+++ tutorial-03/transform.py
@@ -1,3 +1,42 @@
-"""
-Define Rotation Class
-"""
+from dataclasses import dataclass
+
+import numpy as np
+
+
+class Rotation:
+    def __init__(self):
+        pass
+
+    @dataclass
+    def from_euler(self):
+        pass
+
+    def from_quat(self):
+        pass
+
+    def from_rotmat(self):
+        pass
+
+    def from_rotvec(self):
+        pass
+    
+    def as_euler(self):
+        pass
+    
+    def as_quat(self):
+        pass
+
+    def as_rotmat(self):
+        pass
+
+    def as_rotvec(self):
+        pass
+
+    def apply(self):
+        pass
+
+    def identity(self):
+        pass
+
+    def random(self, n: int):
+        pass
