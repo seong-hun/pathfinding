a09a480a1188e2aaefa9fa716e063c576c4e2697
Seong-hun Kim <kshoon92@gmail.com>
2023-07-20 15:54:26 +0900

    Add scripts

--- tutorial-03/main.py
+++ tutorial-03/main.py
@@ -3,3 +3,21 @@ Test Scripts
 """
 
 from transform import Rotation
+
+
+def test_create_quaternion():
+    pass
+
+
+def test_repr_quaternion():
+    pass
+
+
+def test_multiply_quaternion():
+    pass
+
+
+if __name__ == "__main__":
+    test_create_quaternion()
+    test_repr_quaternion()
+    test_multiply_quaternion()
